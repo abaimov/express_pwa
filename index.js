4bf9ee1792edea67d10328094526fd061c2e7f6a
abaimov <w.abaimov@gmail.com>
2024-12-14 19:21:34 +0300

    init

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 8000; // Вы можете изменить порт по вашему усмотрению
+const port = 8000;
 
-app.use(cors()); // Это позволит вашему фронтенду обращаться к API
+app.use(cors());
 
 app.get('/api/link', (req, res) => {
-    // Генерируем случайное число, чтобы симулировать изменение ссылки при каждом запросе
     const randomNumber = Math.floor(Math.random() * 100);
 
     const link = {
@@ -20,4 +19,5 @@ app.get('/api/link', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:${port}`);
+
 });
